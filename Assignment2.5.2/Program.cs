ac2ebfb00cc4c1731f5098bbba725fd66c24f232
neSux0 <senxu98@gmail.com>
2026-07-16 14:27:24 -0700

    test

---
+++ Assignment2.5.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2._5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
